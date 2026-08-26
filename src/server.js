ba98913b68e2c792c5ed256d283000d85b3b8ea0
Uttam demo <chaurasiauttam33@gmail.com>
2026-08-26 13:06:13 +0530

    feat: connect to MongoDB before starting server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const app = require('./app');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Vendora listening on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Vendora listening on port ${PORT}`);
+    });
 });
